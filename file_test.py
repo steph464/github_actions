63775517d5df3504dde69bf9f6b726b536337e63
steph464 <stephanec876@gmail.com>
2023-07-14 14:24:41 +0000

    repair_2 file_test.py

--- file_test.py
+++ file_test.py
@@ -7,7 +7,7 @@ def test_calc_addition():
     assert output == 6
 
 
-def test_calc_subtraction():
+def test_calc_substraction():
     # Fonction test du résultat de 2-4
     output = 2 - 4
     assert output == -2
